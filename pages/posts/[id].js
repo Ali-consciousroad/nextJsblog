fb7964dbe23db305d5ad0ae49e95e1b362258c0b
Ali Dindar <ali_dindar@live.be>
2023-09-13 17:38:14 +0200

    Polishing the Post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,7 @@
+import utilStyles from '../../styles/utils.module.css';
 import Layout from '../../components/layout';
-
+import Head from 'next/head';
+import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 // Use the getPostData function in getStaticProps to get the post data and return it as props
 export async function getStaticProps({ params }) {
@@ -24,13 +26,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
+        <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
     </Layout>
     );
 }
