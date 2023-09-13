1670334ad545cdbc15ea58eebfbfd1a850da288f
Ali Dindar <ali_dindar@live.be>
2023-09-13 11:34:31 +0200

    Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,7 +3,9 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 // Use the getPostData function in getStaticProps to get the post data and return it as props
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id);
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
     return {
         props: {
             postData,
@@ -27,6 +29,8 @@ export default function Post({ postData }) {
         {postData.id}
         <br />
         {postData.date}
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
     );
 }
